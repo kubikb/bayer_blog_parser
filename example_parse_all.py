13fd9e4634ecdbdb26f024fb77c1e9f67ccb3509
Bálint Kubik <kubikbalint@gmail.com>
2018-01-11 22:54:36 +0100

    Cleaning text during parse all, added lemmatized sentences tsv

--- example_parse_all.py
+++ example_parse_all.py
@@ -7,7 +7,14 @@ from bayer_blog_parser.blog_parser import BayerBlogParser
 logging.basicConfig(format='[%(levelname)s] %(asctime)s %(name)s %(filename)s:%(lineno)s - %(message)s',
                     level=logging.DEBUG)
 
+def clean_text(text):
+	return " ".join(text.split())
+
 if __name__ == "__main__":
 	post_data = BayerBlogParser().parse_all_posts()
 	posts_df = pd.DataFrame(post_data)
+
+	posts_df.lead_text = posts_df.lead_text.apply(clean_text)
+	posts_df.full_content = posts_df.full_content.apply(clean_text)
+
 	posts_df.to_csv("data/posts.tsv", sep="\t", encoding="utf-8", index=False)
